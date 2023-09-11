32ce05725545b5d79a33021737045afceac61e78
chetanyaagrawal <chetanyaagrawal@belltech.cc>
2023-09-11 17:22:14 +0530

    new

--- constants.js
+++ constants.js
@@ -1,2 +1,2 @@
-export const Apiurl = "http://13.233.106.110:8000/";
-// export const Apiurl = "http://127.0.0.1:4000/";
+export const Apiurl = "http://api.paisabacha.in/";
+// export const Apiurl = "http://127.0.0.1:4000/";13.126.227.109
